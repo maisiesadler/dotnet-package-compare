aa8042f1c4c8b3b7fe72c453f0b3a04644157f4f
maisiesadler <maisiesadler@hotmail.com>
2023-03-11 10:48:07 +0000

    move type

---
+++ src/PackageLister/ProjectPackagesOutput.cs
@@ -0,0 +1,47 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace PackageLister;
+
+public record ProjectAndFramework(string ProjectName, string FrameworkName);
+
+public class ProjectPackagesOutput
+{
+    private readonly Dictionary<ProjectAndFramework, Dictionary<string, Package>> _packagesByProjectAndFramwork = new();
+
+    public ProjectPackagesOutput(params (string projectName, string frameworkName, Package package)[] initial)
+    {
+        foreach (var project in initial)
+        {
+            Add(project.projectName, project.frameworkName, project.package);
+        }
+    }
+
+    public void Add(string projectName, string frameworkName, Package package)
+    {
+        var projectAndFramework = new ProjectAndFramework(projectName, frameworkName);
+        if (!_packagesByProjectAndFramwork.ContainsKey(projectAndFramework))
+            _packagesByProjectAndFramwork.Add(projectAndFramework, new Dictionary<string, Package>());
+
+        _packagesByProjectAndFramwork[projectAndFramework].Add(package.Name, package);
+    }
+
+    public bool TryGetPackage(ProjectAndFramework projectAndFramework, string packageName, [NotNullWhen(true)] out Package? package)
+    {
+        if (_packagesByProjectAndFramwork.TryGetValue(projectAndFramework, out var packages)
+            && packages.TryGetValue(packageName, out package))
+        {
+            return true;
+        }
+
+        package = null;
+        return false;
+    }
+
+    public IEnumerable<(ProjectAndFramework projectAndFramework, List<Package> package)> GetPackagesByProjectAndFramework()
+    {
+        foreach (var (projectAndFramework, packageKv) in _packagesByProjectAndFramwork)
+        {
+            yield return (projectAndFramework, packageKv.Values.ToList());
+        }
+    }
+}
