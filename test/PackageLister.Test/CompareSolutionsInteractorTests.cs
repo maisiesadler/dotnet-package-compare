354dbd9e7f16be62ceb5051771980c59a3c1be53
maisiesadler <maisiesadler@hotmail.com>
2023-03-12 09:27:47 +0000

    Migrate remaining tests

--- test/PackageLister.Test/CompareSolutionsInteractorTests.cs
+++ test/PackageLister.Test/CompareSolutionsInteractorTests.cs
@@ -39,4 +39,126 @@ public class CompareSolutionsInteractorTests
         Assert.Empty(frameworks.PackageChanges.Added);
         Assert.Empty(frameworks.PackageChanges.Removed);
     }
+
+    [Fact]
+    public async Task AddedPackage_ReturnedAsAdded()
+    {
+        // Arrange
+        _getProjectsBeforeQuery.Setup(q => q.Execute())
+            .ReturnsAsync(new SolutionListPackagesOutput(
+            ("Project1", "net7.0", new Package("Package.One", "1.2.3", true))
+        ));
+
+        _getProjectsAfterQuery.Setup(q => q.Execute())
+            .ReturnsAsync(new SolutionListPackagesOutput(
+            ("Project1", "net7.0", new Package("Package.One", "1.2.3", true)),
+            ("Project1", "net7.0", new Package("Package.Two", "1.2.3", true))
+        ));
+
+        // Act
+        var changes = await _interactor.Execute();
+
+        // Assert
+        var project = Assert.Single(changes);
+        var frameworks = Assert.Single(project.Frameworks);
+        Assert.Empty(frameworks.PackageChanges.Changed);
+        Assert.Empty(frameworks.PackageChanges.Removed);
+        var packagesAdded = Assert.Single(frameworks.PackageChanges.Added);
+
+        Assert.Equal("Package.Two", packagesAdded.Name);
+        Assert.Equal("1.2.3", packagesAdded.Version);
+        Assert.True(packagesAdded.DirectReference);
+    }
+
+    [Fact]
+    public async Task RemovedPackage_ReturnedAsRemoved()
+    {
+        // Arrange
+        _getProjectsBeforeQuery.Setup(q => q.Execute())
+            .ReturnsAsync(new SolutionListPackagesOutput(
+            ("Project1", "net7.0", new Package("Package.One", "1.2.3", true)),
+            ("Project1", "net7.0", new Package("Package.Two", "1.2.3", true))
+        ));
+
+        _getProjectsAfterQuery.Setup(q => q.Execute())
+            .ReturnsAsync(new SolutionListPackagesOutput(
+            ("Project1", "net7.0", new Package("Package.One", "1.2.3", true))
+        ));
+
+        // Act
+        var changes = await _interactor.Execute();
+
+        // Assert
+        var project = Assert.Single(changes);
+        var frameworks = Assert.Single(project.Frameworks);
+        Assert.Empty(frameworks.PackageChanges.Added);
+        Assert.Empty(frameworks.PackageChanges.Changed);
+        var packagesAdded = Assert.Single(frameworks.PackageChanges.Removed);
+
+        Assert.Equal("Package.Two", packagesAdded.Name);
+        Assert.Equal("1.2.3", packagesAdded.Version);
+        Assert.True(packagesAdded.DirectReference);
+    }
+
+    [Fact]
+    public async Task VersionChange_ShownAsChange()
+    {
+        // Arrange
+        _getProjectsBeforeQuery.Setup(q => q.Execute())
+            .ReturnsAsync(new SolutionListPackagesOutput(
+            ("Project1", "net7.0", new Package("Package.One", "1.2.3", true))
+        ));
+
+        _getProjectsAfterQuery.Setup(q => q.Execute())
+            .ReturnsAsync(new SolutionListPackagesOutput(
+            ("Project1", "net7.0", new Package("Package.One", "2.0.0", true))
+        ));
+
+        // Act
+        var changes = await _interactor.Execute();
+
+        // Assert
+        var project = Assert.Single(changes);
+        var frameworks = Assert.Single(project.Frameworks);
+        Assert.Empty(frameworks.PackageChanges.Added);
+        Assert.Empty(frameworks.PackageChanges.Removed);
+        var packagesChanged = Assert.Single(frameworks.PackageChanges.Changed);
+
+        Assert.Equal("Package.One", packagesChanged.Name);
+        Assert.Equal("1.2.3", packagesChanged.VersionBefore);
+        Assert.Equal("2.0.0", packagesChanged.VersionAfter);
+        Assert.True(packagesChanged.DirectReferenceBefore);
+        Assert.True(packagesChanged.DirectReferenceAfter);
+    }
+
+    [Fact]
+    public async Task DirectReferenceChange_ShownAsChange()
+    {
+        // Arrange
+        _getProjectsBeforeQuery.Setup(q => q.Execute())
+            .ReturnsAsync(new SolutionListPackagesOutput(
+            ("Project1", "net7.0", new Package("Package.One", "1.2.3", true))
+        ));
+
+        _getProjectsAfterQuery.Setup(q => q.Execute())
+            .ReturnsAsync(new SolutionListPackagesOutput(
+            ("Project1", "net7.0", new Package("Package.One", "1.2.3", false))
+        ));
+
+        // Act
+        var changes = await _interactor.Execute();
+
+        // Assert
+        var project = Assert.Single(changes);
+        var frameworks = Assert.Single(project.Frameworks);
+        Assert.Empty(frameworks.PackageChanges.Added);
+        Assert.Empty(frameworks.PackageChanges.Removed);
+        var packagesChanged = Assert.Single(frameworks.PackageChanges.Changed);
+
+        Assert.Equal("Package.One", packagesChanged.Name);
+        Assert.Equal("1.2.3", packagesChanged.VersionBefore);
+        Assert.Equal("1.2.3", packagesChanged.VersionAfter);
+        Assert.True(packagesChanged.DirectReferenceBefore);
+        Assert.False(packagesChanged.DirectReferenceAfter);
+    }
 }
