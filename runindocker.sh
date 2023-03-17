a0cd70cbea8301f1b09123392b631598850ff98b
maisiesadler <maisiesadler@hotmail.com>
2023-03-17 08:00:37 +0000

    Update runindocker.sh

--- runindocker.sh
+++ runindocker.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-env
+echo Comparing files $BEFORE_FILE_LOCATION and $AFTER_FILE_LOCATION
 
-result="$(dotnet /PackageLister.Console.dll $BEFORE_FILE_LOCATION $AFTER_FILE_LOCATION)"
-
-r=$?
-if [ $r -ne 0 ]; then
-    echo "Invalid result code"
-    echo $result
-    exit $r
-fi
-
-echo $result
+dotnet /PackageLister.Console.dll $BEFORE_FILE_LOCATION $AFTER_FILE_LOCATION
 
 EOF=$(dd if=/dev/urandom bs=15 count=1 status=none | base64)
-    echo "DIFF_OUTPUT<<$EOF" >> $GITHUB_ENV
-    echo $result >> $GITHUB_ENV
-    echo "$EOF" >> $GITHUB_ENV
+echo "DIFF_OUTPUT<<$EOF" >> $GITHUB_ENV
+dotnet /PackageLister.Console.dll $BEFORE_FILE_LOCATION $AFTER_FILE_LOCATION >> $GITHUB_ENV
+echo "$EOF" >> $GITHUB_ENV
